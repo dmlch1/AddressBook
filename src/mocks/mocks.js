ff0b2767a82d6670aca474a629b336f748b3f016
dmlch1 <66270979+dmlch1@users.noreply.github.com>
2022-11-08 09:38:42 +0400

    AddressBook

---
+++ src/mocks/mocks.js
@@ -0,0 +1,77 @@
+const Mocks = () => {
+    const mocks = [{
+        "id": 1,
+        "name": 'Жена',
+        "phone": '+7(906)142-34-85',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 2,
+        "name": 'Колян Цемент',
+        "phone": '+7(906)523-34-85',
+        "favorite": 'none'
+    },
+    {
+        "id": 3,
+        "name": 'Серёга Масло',
+        "phone": '+7(928)142-44-85',
+        "favorite": 'none'
+    },
+    {
+        "id": 4,
+        "name": 'Танюха Питер',
+        "phone": '+7(906)152-37-55',
+        "favorite": 'none'
+    },
+    {
+        "id": 5,
+        "name": 'Сварщик порогов',
+        "phone": '+7(906)442-34-55',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 6,
+        "name": 'Сиплый Тюмень',
+        "phone": '+7(928)142-44-85',
+        "favorite": 'none'
+    },{
+        "id": 7,
+        "name": 'Брат Серёга',
+        "phone": '+7(906)142-36-85',
+        "favorite": 'none'
+    },
+    {
+        "id": 8,
+        "name": 'Кредит',
+        "phone": '+7(906)42-54-35',
+        "favorite": 'none'
+    },
+    {
+        "id": 9,
+        "name": 'Не отвечать',
+        "phone": '+7(958)342-15-35',
+        "favorite": 'none'
+    },
+    {
+        "id": 10,
+        "name": 'Лёха стройка',
+        "phone": '+7(936)412-54-83',
+        "favorite": 'none'
+    },
+    {
+        "id": 11,
+        "name": 'Ислам Махачкала',
+        "phone": '+7(936)51-44-11',
+        "favorite": 'none'
+    },
+    {
+        "id": 12,
+        "name": 'Ильнур челик',
+        "phone": '+7(991)432-43-85',
+        "favorite": 'favorite'
+    }]
+
+    return mocks
+}
+
+export default Mocks
